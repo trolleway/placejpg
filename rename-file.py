608c7d5845fb3770c538589ea715a6c16fd06c40
Artem Svetlov <trolleway@yandex.ru>
2024-01-24 01:26:11 +0300

    new: remaner files

---
+++ rename-file.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os, subprocess, logging, argparse, sys, pprint, datetime
+import pywikibot
+
+import trolleway_commons
+from model_wiki import Model_wiki
+from fileprocessor import Fileprocessor
+from urllib.parse import urlparse
+
+
+parser = argparse.ArgumentParser(
+    description=" ")
+
+parser.add_argument('--pagename', type=str, required=False, help='Wikipedia filepage')
+parser.add_argument('--wikidata', type=str, required=False)
+parser.add_argument("--verify", action="store_const",
+                    required=False, default=False, const=True)
+
+class Helper_rename:
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)-8s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    logger = logging.getLogger(__name__)
+    pp = pprint.PrettyPrinter(indent=4)
+    
+    cachedir='maintainer_cache'
+    fileprocessor = Fileprocessor()
+
+    def dowload_or_cache_read(self,FilePage)->str:
+        if not os.path.isdir(self.cachedir):
+            os.makedirs(self.cachedir)
+            
+        url = FilePage.get_file_url()   
+        pageid = FilePage.pageid
+        ext = os.path.splitext(os.path.basename(urlparse(url).path))[1]
+        fn=os.path.splitext(os.path.basename(urlparse(url).path))[0]
+        filepath = os.path.join(self.cachedir, os.path.basename(urlparse(url).path) )
+        filepath = os.path.join(self.cachedir,fn+ext )
+        if os.path.isfile(filepath):
+            return filepath
+        FilePage.download(filename=filepath)
+        return filepath
+     
+    
+    def get_dt_from_FilePage(self,filepage)->datetime.datetime:
+        local_filepath=self.dowload_or_cache_read(filepage)
+        modelwiki = Model_wiki()
+        dt_obj=self.fileprocessor.image2datetime(local_filepath)
+        return dt_obj
+    
+    def generate_filename(self,pagename,wikidata)->str:
+        
+        assert pagename
+        # Login to your account
+        site = pywikibot.Site('commons', 'commons')
+        site.login()
+        site.get_tokens("csrf")  # preload csrf token
+
+        file_page = pywikibot.FilePage(site, pagename)
+
+        dt_obj = helper_renamer.get_dt_from_FilePage(file_page)
+
+        objectnames=modelwiki.get_wikidata_simplified(wikidata)['labels']
+        filename=file_page.get_file_url()
+        commons_filename = self.fileprocessor.commons_filename(
+                filename, objectnames, wikidata, dt_obj,add_administrative_name=False)
+        return commons_filename
+    
+    def generate_rename_template(self,new_name:str)->str:
+        text='{{Rename|1='+new_name+'|2=2|3=human-readable name from wikidata name}}'
+        return text
+    
+    def prepend_text(self,text,new_text,check='')->str:
+        if check in text:
+            print(check+' already in text')
+            return None
+        text = new_text + "\n"+text
+        return text
+    
+    def prepend_text_page(self,pagename,rename_template_text):
+        assert pagename
+        # Login to your account
+        site = pywikibot.Site('commons', 'commons')
+        site.login()
+        site.get_tokens("csrf")  # preload csrf token
+        file_page = pywikibot.FilePage(site, pagename)
+
+        new_page_text = helper_renamer.prepend_text(file_page.text,rename_template_text,'{{Rename')
+        if new_page_text is None:
+            return None
+        page_not_need_change = True
+        if new_page_text!=file_page.text:page_not_need_change = False
+
+        file_page.text = new_page_text
+        if page_not_need_change == False:
+            file_page.save('propose rename')
+
+        return True        
+
+
+if __name__ == '__main__':
+    
+    args = parser.parse_args()
+    processor = trolleway_commons.CommonsOps()
+    modelwiki = Model_wiki()
+    helper_renamer = Helper_rename()
+
+    pagename=args.pagename
+    wikidata= modelwiki.wikidata_input2id(args.wikidata)
+    
+    new_name = helper_renamer.generate_filename(pagename,wikidata)
+    rename_template_text = helper_renamer.generate_rename_template(new_name)
+    
+    verify=args.verify
+    mode = 'change'
+    if mode=='change':
+        if verify:
+            print('prepend string')
+            print(rename_template_text)
+            print("Press any key to continue or Ctrl+C for cancel...")
+            input()
+        helper_renamer.prepend_text_page(pagename,rename_template_text)
+    
+    
+
+    
+        
